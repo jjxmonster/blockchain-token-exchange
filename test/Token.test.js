10a2e461367f72ce74fc9e1c1be0a84422bea832
jjxmonster <kuba.tarabasz@wp.pl>
2022-04-06 22:40:16 +0200

    transfer tokens function and tests

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,13 +1,15 @@
+import { tokens, EVM_REVERT } from './helpers.js';
+
 /* eslint-disable no-undef */
 const Token = artifacts.require('./Token');
 
 require('chai').use(require('chai-as-promised')).should();
 
-contract(Token, accounts => {
-   const name = 'My name';
-   const symbol = 'Symbol';
-   const decimals = 10;
-   const totalSupply = 10;
+contract(Token, ([deployer, receiver]) => {
+   const name = 'JJ Token';
+   const symbol = 'JJ';
+   const decimals = '18';
+   const totalSupply = tokens(1000000).toString();
    let token;
 
    beforeEach(async () => {
@@ -17,7 +19,6 @@ contract(Token, accounts => {
    describe('deployment', () => {
       it('tracks the name', async () => {
          const result = await token.name();
-         console.log(result);
          result.should.equal(name);
       });
 
@@ -27,11 +28,60 @@ contract(Token, accounts => {
       });
       it('tracks the decimals', async () => {
          const result = await token.decimals();
-         result.should.equal(decimals);
+         result.toString().should.equal(decimals);
       });
       it('tracks the total supply', async () => {
          const result = await token.totalSupply();
-         result.should.equal(totalSupply);
+         result.toString().should.equal(totalSupply.toString());
+      });
+
+      it('assigns the total supply to the deployer', async () => {
+         const result = await token.balanceOf(deployer);
+         result.toString().should.equal(totalSupply.toString());
+      });
+   });
+
+   describe('sending tokens', () => {
+      let result;
+      let amount;
+
+      describe('success', async () => {
+         beforeEach(async () => {
+            amount = tokens(100);
+            result = await token.transfer(receiver, amount, {
+               from: deployer,
+            });
+         });
+
+         it('transfers token balances', async () => {
+            let balanceOf;
+
+            balanceOf = await token.balanceOf(deployer);
+            balanceOf.toString().should.equal(tokens(999900).toString());
+            balanceOf = await token.balanceOf(receiver);
+            balanceOf.toString().should.equal(tokens(100).toString());
+         });
+
+         it('emits a transfer event', async () => {
+            const log = result.logs[0];
+            log.event.should.equal('Transfer');
+            const event = log.args;
+
+            event.from.toString().should.equal(deployer, 'from is correct');
+            event.to.toString().should.equal(receiver, 'to is correct');
+            event.value.toString().should.equal(amount.toString());
+         });
+      });
+
+      describe('failure', async () => {
+         it('rejects insufficient balances', async () => {
+            let invalidAmount;
+            invalidAmount = tokens(100000000); // 100 millions - greater than total should
+
+            await token
+               .transfer(receiver, invalidAmount, { from: deployer })
+               .should.be.rejectedWith(EVM_REVERT);
+         });
       });
    });
 });
