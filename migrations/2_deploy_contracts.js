8e6925b2ff6cc58a82e607e36418b34678ecba3f
jjxmonster <kuba.tarabasz@wp.pl>
2022-04-14 20:34:02 +0200

    frontend

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,18 @@
-// eslint-disable-next-line no-undef
-const Token = artifacts.require('Token');
+const Web3 = require('web3');
 
-module.exports = function (deployer) {
-   deployer.deploy(Token);
+const web3 = new Web3('HTTP://127.0.0.1:7545');
+
+/* eslint-disable no-undef */
+const Token = artifacts.require('Token');
+const Exchange = artifacts.require('Exchange');
+
+module.exports = async function (deployer) {
+   const accounts = await web3.eth.getAccounts();
+
+   await deployer.deploy(Token);
+
+   const feeAccount = accounts[0];
+   const feePercent = 10;
+
+   await deployer.deploy(Exchange, feeAccount, feePercent);
 };
--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -58,5 +58,3 @@ contract Token {
     }
 
 }
-
- 
